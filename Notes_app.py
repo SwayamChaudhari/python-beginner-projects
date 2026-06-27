852b525e93cb9045709f9af51887a3d7ee882ce8
Swayam Chaudhari <swayam.chaudhari02@gamil.com>
2026-06-27 10:03:44 +0530

    Added Notes App project

---
+++ Notes_app.py
@@ -0,0 +1,27 @@
+# Press 1 for Adding Note 
+# Press 2 for Display Note
+# Press 3 to Exit 
+while True:
+    print('''===== Notes App =====
+        1. Add Note
+        2. Show Notes
+        3. Exit''')
+    try:
+        user = int(input("Enter a number -> "))     
+    except:
+        print("Enter a valid number")
+        continue
+    if user == 1:
+        note = input("Enter note -> ")
+        with open("notes.txt","a") as f:
+         f.write(note + "\n")
+         print("✅ Note Saved Successfully")
+    elif user == 2:
+        with open("notes.txt", "r") as f1:
+         print(f1.read())
+    elif user == 3:
+        print("Exit")
+        break
+    else:
+        print("Wrong Input")
+
